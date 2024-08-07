4b297b4878eab4beef6948430bd27f3f331621fd
Christian <cortiz2894@gmail.com>
2024-08-06 21:41:11 -0300

    change ts on productList - test-

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,8 +1,8 @@
-'use client'
+'use client';
+import React, { useEffect, useRef, useState } from 'react';
 import ProductCard from "@/components/ProductCard/index";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import { useEffect, useRef, useState } from "react";
 import { Product } from "../ProductCard/product.types";
 
 const PRODUCT_LIST = [
@@ -13,54 +13,54 @@ const PRODUCT_LIST = [
     description: `Our "Reign Of Blood Shorts" in black & white are the perfect addition to our already legendary perfect`,
     image: 't-shirt-1.png'
   },
-	{
+  {
     id: 2,
     title: 'Reign Of Blood 2.0 - Boardshorts',
     price: 99,
     description: `Our "Reign Of Blood Shorts" in black & white are the perfect addition to our already legendary perfect`,
     image: 'hoodie.png'
   },
-	{
+  {
     id: 3,
     title: 'Reign Of Blood 2.0 - Boardshorts',
     price: 99,
     description: `Our "Reign Of Blood Shorts" in black & white are the perfect addition to our already legendary perfect`,
     image: 'gorra.png'
   },
-	{
+  {
     id: 5,
     title: 'Reign Of Blood 2.0 - Boardshorts',
     price: 99,
     description: `Our "Reign Of Blood Shorts" in black & white are the perfect addition to our already legendary perfect`,
     image: 'shorts.png'
   }
-]
+];
 
 gsap.registerPlugin(ScrollTrigger);
 
-
 export default function ProductList() {
-  const [products, setProducts] = useState<Product[]>(PRODUCT_LIST)
-  const productRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const [products, setProducts] = useState(PRODUCT_LIST);
+  const productRefs = useRef([]);
 
-	useEffect(() => {
+  useEffect(() => {
     gsap.fromTo(
       productRefs.current,
       { opacity: 0, y: 100 },
-      { opacity: 1, y: 0 , stagger: 0.3, duration: 0.5, scrollTrigger: productRefs.current }
+      { opacity: 1, y: 0, stagger: 0.3, duration: 0.5, scrollTrigger: productRefs.current }
     );
   }, []);
 
   return (
-        <div className="grid grid-cols-4 gap-5">
-					{products.map((item, index) => {
-						return(
-							<ProductCard 
-								ref={(el) => (productRefs.current[index] = el)}
-								key={`item-${item.id}`} item={item}
-							/>
-						)
-					})}
-        </div>
+    <div className="grid grid-cols-4 gap-5">
+      {products.map((item, index) => {
+        return (
+          <ProductCard
+            ref={(el) => (productRefs.current[index] = el)}
+            key={`item-${item.id}`}
+            item={item}
+          />
+        );
+      })}
+    </div>
   );
 }
