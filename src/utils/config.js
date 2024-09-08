04e852643f1b27f52435f10e92be1981738b413e
Christian <cortiz2894@gmail.com>
2024-09-08 09:07:17 -0300

    change apiUrl

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,8 @@
-export const API_URL = process.env.API_URL || 'http://127.0.0.1:1337';
+export const API_URL = process.env.NODE_ENV === 'production' 
+? 'https://innovative-hope-c0b50683e6.strapiapp.com/' 
+: 'http://127.0.0.1:1337';
+
+// process.env.API_URL || 'http://127.0.0.1:1337';
 
 export const formattedProductsResponse = (products) => {
   return products.map(({attributes, id}) => {
