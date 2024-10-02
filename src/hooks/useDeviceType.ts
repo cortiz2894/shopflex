531bb3addb7660a9a2dec51e7bebd472e6a921f0
Nicolas Agustin Perez <nicolasperez95.a@gmail.com>
2024-10-02 12:02:52 -0300

    feat: removed error on the server side

--- src/hooks/useDeviceType.ts
+++ src/hooks/useDeviceType.ts
@@ -2,10 +2,16 @@ import { useState, useEffect } from 'react';
 
 const useDeviceType = () => {
   const [isMobile, setIsMobile] = useState(() => {
+    if (!window) {
+      return;
+    }
     return window.innerWidth <= 768;
   });
 
   useEffect(() => {
+    if (!window) {
+      return;
+    }
     const mediaQuery = window.matchMedia('(max-width: 768px)');
 
     const handleResize = (e: MediaQueryListEvent) => {
