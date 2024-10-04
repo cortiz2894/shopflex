327bcb3c6242dd3b2d8ad8c3a082fb59481dec18
Christian Daniel Ortiz Ororbia <45889252+cortiz2894@users.noreply.github.com>
2024-10-03 22:07:05 -0300

    [UI] - Responsive v1 (#2)
    * feat: navlink responsive
    
    * feat: Hero responsive | Infinite text responsive
    
    * feat: improve infinite text position on mobile
    
    * feat: Products responsive v1
    
    * feat: ProductCard responsive
    
    * feat: unable hover on mobile in productsCards
    
    * fix bugs on product description
    
    * feat: z-index menu
    
    * feat: improve swipe animation
    
    * feat: collections responsive

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -7,7 +7,7 @@ interface Props extends PropsWithChildren {
 export default function Container({children}:Props) {
 
   return (
-    <div className="flex justify-center my-10 w-full z-10">
+    <div className="flex justify-center my-5 md:my-10 w-full z-10">
         <div className="w-[90%]">
             {children}
         </div>
--- src/components/Hero/InfiniteText/index.tsx
+++ src/components/Hero/InfiniteText/index.tsx
@@ -10,60 +10,62 @@ gsap.registerPlugin(ScrollTrigger);
 
 type Props = {
   text: string,
-	size?: 'small'| 'medium',
-	position?: 'hero' | 'bottom'
-  controls?: boolean
+  size?: 'small' | 'medium',
+  position?: 'hero' | 'bottom',
+  controls?: boolean,
+  direction?: 'left' | 'right'
 }
 
-export default function InfiniteText({text, size = 'medium', position = 'hero', controls = false}: Props) {
+export default function InfiniteText({ text, size = 'medium', position = 'hero', controls = false, direction = 'left' }: Props) {
 
-	const firstText = useRef(null);
-	const secondText = useRef(null);
-	const slider = useRef(null);
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const slider = useRef(null);
 
-	let xPercent = 0;
-	let direction = -1;
+  let xPercent = 0;
+  let scrollDirection = direction === 'left' ? -1 : 1;
 
-  useEffect( () => {
-	if(!slider.current) return
-    if(controls) {
+  useEffect(() => {
+    if (!slider.current) return;
+
+    if (controls) {
       gsap.to(slider.current, {
         scrollTrigger: {
           trigger: document.documentElement,
           scrub: 0.5,
           start: 0,
           end: window.innerHeight,
-          onUpdate: e => direction = e.direction * -1
+          onUpdate: e => scrollDirection = direction === 'left' ? e.direction * -1 : e.direction * 1  // Ajuste de la dirección basado en el scroll
         },
-        x: "-500px",
-      })
+        x: direction === 'left' ? "-500px" : "500px",  // Movimiento inicial
+      });
     }
-    requestAnimationFrame(animate);	  
-  }, [])
+
+    requestAnimationFrame(animate);
+  }, [controls, direction]);
 
   const animate = () => {
-	if(!firstText.current) return
-	if(!secondText.current) return
+    if (!firstText.current || !secondText.current) return;
 
-		if(xPercent < -100){
-		  xPercent = 0;
-		}
-		else if(xPercent > 0){
-		  xPercent = -100;
-		}
+    if (xPercent < -100) {
+      xPercent = 0;
+    } else if (xPercent > 0) {
+      xPercent = -100;
+    }
 
-		gsap.set(firstText.current, {xPercent: xPercent})
-		gsap.set(secondText.current, {xPercent: xPercent})
-		requestAnimationFrame(animate);
-		xPercent += 0.1 * direction;
-  }
+    gsap.set(firstText.current, { xPercent: xPercent });
+    gsap.set(secondText.current, { xPercent: xPercent });
+
+    requestAnimationFrame(animate);
+    xPercent += 0.1 * scrollDirection;
+  };
 
   return (
-		<div className={classNames(styles.sliderContainer, [styles[size]], [styles[position]])}>
-			<div ref={slider} className={styles.slider}>
-				<p ref={firstText} >{text}</p>
-				<p ref={secondText}>{text}</p>
-			</div>
-		</div>
+    <div className={classNames(styles.sliderContainer, [styles[size]], [styles[position]])}>
+      <div ref={slider} className={styles.slider}>
+        <p ref={firstText}>{text}</p>
+        <p ref={secondText}>{text}</p>
+      </div>
+    </div>
   );
 }
