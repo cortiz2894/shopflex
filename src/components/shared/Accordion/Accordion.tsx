f83524884841dc7ea75a273dd1a47a63ad1d2c07
Nicolas Agustin Perez <nicolasperez95.a@gmail.com>
2024-10-16 22:05:29 -0300

    fix: fixed accordeon bug

---
+++ src/components/shared/Accordion/Accordion.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { ReactNode, useEffect, useRef, useState } from 'react';
+import classNames from 'classnames';
+import styles from './Accordion.module.scss';
+import gsap from 'gsap';
+
+interface Props {
+  title: string;
+  content: ReactNode;
+  icon?: React.ReactNode;
+}
+
+export const Accordion = ({ title, content, icon }: Props) => {
+  const [active, setActive] = useState<boolean>(false);
+  const contentRef = useRef<HTMLDivElement | null>(null);
+  const animationRef = useRef<gsap.core.Tween | null>(null);
+
+  useEffect(() => {
+    if (!contentRef.current) return;
+    animationRef.current = gsap
+      .to(contentRef.current, {
+        height: 'auto',
+        ease: 'power1.inOut',
+        paused: true,
+        duration: 0.35,
+      })
+      .reverse();
+  }, []);
+
+  const handleClick = () => {
+    if (!animationRef.current) return;
+    animationRef.current.reversed(!animationRef.current.reversed());
+    setActive(!active);
+  };
+
+  return (
+    <div className={classNames('mb-3', [styles.accordion])}>
+      <div
+        className={classNames([styles.header], 'bg-white rounded-lg flex justify-between px-3 py-3 items-center')}
+        onClick={handleClick}
+      >
+        <div className="flex gap-3 justify-center items-center">
+          {icon}
+          <p className="text-standar-darker font-semibold text-lg">{title}</p>
+        </div>
+        <div className={classNames([styles.plusminus], { [styles.active]: active })}></div>
+      </div>
+      <div className={classNames('bg-white', [styles.content])} ref={contentRef}>
+        <div className="text-standar-darker p-3">{content}</div>
+      </div>
+    </div>
+  );
+};
